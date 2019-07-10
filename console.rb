f342b291578fbc6ad25578c24ed61422c4449f9e
marmalou <marmalou2@gmail.com>
2019-07-10 15:44:55 +0100

    console.rb

---
+++ console.rb
@@ -0,0 +1,20 @@
+require("pry")
+require_relative('../models/album')
+require_relative('../models/artist')
+
+Album.delete_all()
+Artist.delete_all()
+
+artist1 = Artist.new( {'name' => 'Bjork'})
+artist1.save()
+
+album1 = Album.new( {
+  'title' => 'Vespertine',
+  'genre' => 'Avant-Garde',
+  'artist_id' => artist1.id
+  })
+  album1.save()
+
+binding.pry
+
+nil
